3b0fc4e59332d1eae4b2bf4cb7758ee7bde30be5
GrabowskiDev <lukaszgra2004@gmail.com>
2021-05-03 19:58:53 +0200

    add auth

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+/* FIREBASE AUTHENTICATION */
+const auth = firebase.auth();
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+/// Sign in event handlers
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        signInBtn.hidden = true;
+        signOutBtn.hidden = false;
+    } else {
+        // not signed in
+        signInBtn.hidden = false;
+        signOutBtn.hidden = true;
+    }
+});
+
+
 //Declaring user library
 let myLibrary = []
 
